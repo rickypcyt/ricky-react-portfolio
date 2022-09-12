65f2d9f048a3756a1edc0a0af2419f91e1f6c273
rickypcyt <rickypcyt@gmail.com>
2022-09-12 12:48:35 -0500

    Changes4

---
+++ .history/src/pages/Projects_20220912123346.js
@@ -0,0 +1,110 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import webmurals from "../images/webmurals.png"; // gives image path
+import notesjs from "../images/notesjs.jpg"; // gives image path
+
+
+export default function Projects() {
+  return (
+    <main>
+      <main className="container">
+        <div className="cabecera">
+          <h1 className="titulo">Projects </h1>
+          <p className="descripcion">These are my small proyects</p>
+        </div>
+        <div className="nuevoflex">
+          <div className="fixer">
+            <section className="imagenesproyect ">
+              <Link to="../webmural">
+                <img className="imagenes" src={webmurals} alt="this is car" />
+                <div class="overlay">Web Mural</div>
+                <div class="overlay2">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </div>
+              </Link>
+            </section>
+            <section className="imagenesproyect ">
+              <Link to="../notesjs">
+                <img className="imagenes" src={notesjs} alt="this is car" />
+                <div class="overlay">Notes</div>
+                <div class="overlay2">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </div>
+              </Link>
+            </section>
+          </div>
+          <div className="fixer">
+            <section className="imagenesproyect ">
+              <Link to="../info">
+                <img className="imagenes" src={webmurals} alt="this is car" />
+                <div class="overlay">Web Mural</div>
+                <div class="overlay2">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </div>
+              </Link>
+            </section>
+            <section className="imagenesproyect ">
+              <Link to="../info">
+                <img className="imagenes" src={webmurals} alt="this is car" />
+                <div class="overlay">Web Mural</div>
+                <div class="overlay2">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </div>
+              </Link>
+            </section>
+          </div>
+          <div className="fixer">
+            <section className="imagenesproyect ">
+              <Link to="../info">
+                <img className="imagenes" src={webmurals} alt="this is car" />
+                <div class="overlay">Web Mural</div>
+                <div class="overlay2">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </div>
+              </Link>
+            </section>
+            <section className="imagenesproyect ">
+              <Link to="../info">
+                <img className="imagenes" src={webmuralss} alt="this is car" />
+                <div class="overlay">Web Mural</div>
+                <div class="overlay2">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </div>
+              </Link>
+            </section>
+          </div>
+          <nav className="cabeceranavi">
+            <ol>
+              <CustomLink to="/" className="oli">
+                Home
+              </CustomLink>
+            </ol>
+          </nav>
+        </div>
+      </main>
+    </main>
+  );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  );
+}
